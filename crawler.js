a3fdffe244a6b1911a2b68eb5cd4649bb9c83681
Vitor Giovanni Cominato de Amorim <vitor.cominato@gmail.com>
2017-10-13 15:53:10 -0300

    Funcionando

--- crawler.js
+++ crawler.js
@@ -1,26 +1,53 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
-var GitHub = require('github-api');
-var Promise = require("es6-promise").Promise;
 
-// basic auth 
-var gh = new GitHub({
-  username: 'vitorcominato',
-  password: 'c0cac0la',
+var token = "464adba848db3ff77c490a2918a504bee6e31a7c";
+var baseUrl = "http://github.amil.com.br/api/v3/";
+var urlSufix = "?access_token=" + token;
+var pageToVisit = baseUrl + "user/repos?access_token="+token;
+
+
+
+request(pageToVisit, function(error, response, body) {
+  var repositories = JSON.parse(response.body)
+  Object.keys(repositories).map(function(objectKey, index) {
+    var repo = repositories[objectKey];
+    branchesUrl = baseUrl + "repos/clinical/" + repo.name + "/branches" + urlSufix
+    request(branchesUrl, function(error, response, body) {
+      var branches = JSON.parse(response.body)
+      // ssconsole.log(branches);
+      Object.keys(branches).map(function(objectKey, index) {
+        var branch = branches[objectKey];
+         // console.log(branch.name);
+        if(branch.name != "master"){
+          pullsUrl = baseUrl + "repos/clinical/" + repo.name + "/pulls" + urlSufix
+          var hasPullRequest = false;
+          request(pullsUrl, function(error, response, body) {
+            var pulls = JSON.parse(response.body)
+            Object.keys(pulls).map(function(objectKey, index) {
+              var pull = pulls[objectKey];
+              if(pull.head.ref == branch.name){
+                hasPullRequest = true;
+                
+                issueUrl = baseUrl + "repos/clinical/" + repo.name + "/issues/" + pull.number + urlSufix
+                request(issueUrl, function(error, response, body) {
+                  var issue = JSON.parse(response.body)
+                  Object.keys(issue).map(function(objectKey, index) {
+                    var issueInfo = issue[objectKey];
+                    
+                  });
+                });
+
+                console.log(pull);
+              }
+            });
+            if(!hasPullRequest){
+              // console.log("Repositório " + repo.name + " na " + branch.name + " não tem pull request!");
+            }
+          });
+        }
+      });
+    });
+  });
 });
-
-
-var me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
-console.log(me);
-me.listNotifications(function(err, notifications) {
-   console.log(notifications);
-}).catch(function(){
-  console.log('teste');
-});
-
-// var vitamorim = gh.getUser('vitamorim');
-// vitamorim.listStarredRepos(function(err, repos) {
-//    // look at all the starred repos!
-//    console.log(err);
-// });
